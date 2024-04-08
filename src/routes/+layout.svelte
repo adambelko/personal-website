aedfde3e6f805e5b0d9fe3640a73164fcde9975e
Adam Belko <adam.belko@archa.com.au>
2024-04-08 21:26:32 +1000

    Add basic template and first iteration of navbar component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "$lib/styles/main.css"
+    import Nav from "./Nav.svelte"
+</script>
+
+<Nav />
+
+<main>
+    <slot />
+</main>
---
+++ src/routes/Nav.svelte
@@ -0,0 +1,53 @@
+<nav class="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="/">ADAM BELKO</a>
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a href="/articles" class="nav-link">ARTICLES</a>
+            </li>
+            <li class="nav-item">
+                <a href="/contact" class="nav-link">CONTACT</a>
+            </li>
+            <li class="nav-item">
+                <a href="/about" class="nav-link">ABOUT</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<style>
+    .navbar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #FAFAFA;
+        flex-wrap: nowrap;
+        width: 100vw;
+        height: 125px;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        max-width: 960px;
+        width: 100%;
+        flex-wrap: inherit;
+    }
+
+    .navbar-brand {
+        font-size: 1.3em;
+        font-weight: bold;
+    }
+
+    .navbar-nav {
+        display: flex;
+        gap: 1em;
+        font-size: 0.8em;
+        font-weight: 600;
+    }
+
+    a {
+        color: #323232;
+    }
+</style>
