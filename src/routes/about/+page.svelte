e7d0dc0a6a3516725bce4ca8747354474f8486ca
Adam Belko <adam.belko@archa.com.au>
2024-04-10 10:32:58 +1000

    add tech stack section to About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,17 +1,35 @@
 <div class="about-container">
     <div class="intro-container">
-        <div class="into-text-container">
+        <div class="intro-text-container">
             <h1 class="about-h1">Who Is Adam Belko?</h1>
-            <p>Hey there! Put simply, I enjoy making things for the web, specialising in Front-End side of
-                development.</p>
+            <p>
+                Hey there! Put simply, I enjoy making things for the web, specialising in Front-End side of
+                development.
+            </p>
             <br>
-            <p>When it comes to learning new technologies, my approach typically goes like this: read docs, experiment
+            <p>
+                When it comes to learning new technologies, my approach typically goes like this: read docs, experiment
                 (build projects), hitting issues, and then solving them. In other words I believe reading books or
-                watching YouTube tutorials only get you so far.</p>
+                watching YouTube tutorials only get you so far.
+            </p>
+            <br>
         </div>
         <div class="into-img-container">
             <img src="/my-profile-photo.jpeg" alt="adam-belko">
         </div>
+    </div>
+    <div class="tech-stack-container">
+        <p>Here are a few technologies I’ve been working with recently:</p>
+        <ul class="tech-stack-list">
+            <li>Javascript (ES6+)</li>
+            <li>TypeScript</li>
+            <li>React and Next.js</li>
+            <li>Svelte and SvelteKit</li>
+            <li>Node.js</li>
+            <li>MongoDB</li>
+            <li>Git</li>
+            <li>GitHub</li>
+        </ul>
     </div>
 </div>
 
@@ -27,15 +45,16 @@
         display: flex;
     }
 
-    .into-text-container {
+    .intro-text-container {
         display: flex;
         flex-direction: column;
         width: 50%;
-        padding: 20px 12px;
+        padding-top: 20px;
+        padding-right: 12px;
     }
 
     .into-img-container {
-        padding: 0 12px;
+        padding-left: 12px;
     }
 
     .about-h1 {
@@ -47,10 +66,28 @@
         padding-bottom: 0.4em;
     }
 
-    .into-text-container p {
+    .intro-text-container p{
         font-size: 1.2em;
-        font-weight: 500;
         line-height: 1.5em;
+    }
+
+    .tech-stack-container p {
+        font-weight: bold;
+    }
+
+    .tech-stack-container {
+        margin-top: 1.5em;
+    }
+
+    .tech-stack-list {
+        display: grid;
+        grid-template-columns: repeat(2, minmax(200px, 270px));
+        list-style: disc;
+        font-size: 1em;
+        font-weight: 400;
+        line-height: 1.5em;
+        margin-top: 1em;
+        padding-left: 1.5em;
     }
 
 </style>
