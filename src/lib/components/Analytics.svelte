c98aab01653dee94514e32005d06c944f39ceeb2
Adam Belko <adam.belko@archa.com.au>
2024-10-31 05:56:31 +1100

    add Umami analytics

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,17 +1,18 @@
 <script>
 	import { page } from "$app/stores"
 
-	$: {
+	$effect(() => {
 		if (typeof gtag !== "undefined") {
 			gtag("config", "MEASUREMENT_ID", {
 				page_title: document.title,
 				page_path: $page.url.pathname
 			})
 		}
-	}
+	})
 </script>
 
 <svelte:head>
+	<!--	Google analytics-->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YHNF7ZVTRG"></script>
 	<script>
 		window.dataLayer = window.dataLayer || []
@@ -24,4 +25,11 @@
 
 		gtag("config", "G-YHNF7ZVTRG")
 	</script>
+
+	<!--	Umami analytics-->
+	<script
+		defer
+		src="https://cloud.umami.is/script.js"
+		data-website-id="7cb7f15e-6025-4099-930e-ff8601709f53"
+	></script>
 </svelte:head>
